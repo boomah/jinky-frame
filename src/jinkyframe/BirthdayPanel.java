b06711bd5b6ed7404eb692bc119a56c62a2baa38
boomah <nickdarcy@gmail.com>
2023-01-09 22:29:58 +0000

    separate out the next birthday and highlight it when there are less than five days remaining

--- src/jinkyframe/BirthdayPanel.java
+++ src/jinkyframe/BirthdayPanel.java
@@ -2,11 +2,14 @@ package jinkyframe;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.time.temporal.ChronoUnit.DAYS;
 import static jinkyframe.Colours.black;
+import static jinkyframe.Colours.orange;
 import static jinkyframe.ImageUtils.*;
 
 public final class BirthdayPanel {
@@ -20,12 +23,14 @@ public final class BirthdayPanel {
     public static BufferedImage generate(Info info, Margins margins) {
         var birthdays = info.birthdayInfo().birthdays();
         var currentDate = info.systemInfo().currentTime().toLocalDate();
-        int currentDay = currentDate.getDayOfYear();
+        int currentDayOfYear = currentDate.getDayOfYear();
         int startIndex = IntStream.range(0, birthdays.size())
-                .filter(i -> birthdays.get(i).day().getDayOfYear() > currentDay)
+                .filter(i -> birthdays.get(i).day().getDayOfYear() > currentDayOfYear)
                 .findFirst()
                 .orElse(0);
         int number = 5;
+
+        int width = 120;
 
         var title = string("Upcoming Birthdays");
 
@@ -42,13 +47,20 @@ public final class BirthdayPanel {
                     return String.format("%s %s:  %s", dayString, extraSpace, names);
                 })
                 .toList();
-        var lines = birthdayStrings.stream().map(BirthdayPanel::string).toList();
+        var lines = birthdayStrings.stream().map(BirthdayPanel::string).collect(Collectors.toList());
+        var nextBirthday = birthdays.get(startIndex);
+        long daysUntilNext = daysUntilNext(currentDate, nextBirthday.day().withYear(currentDate.getYear()));
+        boolean soon = daysUntilNext < 5;
+        var nextBirthdayImage = border(lines.remove(0), 4, soon);
 
-        return createImage(120, 86, g -> {
+        return createImage(width, 86, g -> {
             g.drawImage(title, margins.left(), margins.top(), null);
-            int y = title.getHeight() + margins.top() * 3;
+
+            g.drawImage(nextBirthdayImage, margins.left(), title.getHeight() + 10, null);
+
+            int y = title.getHeight() + 30;
             for (BufferedImage line : lines) {
-                g.drawImage(line, margins.left() + 2, y, null);
+                g.drawImage(line, margins.left() + 4, y, null);
                 y += 12;
             }
         });
@@ -56,5 +68,25 @@ public final class BirthdayPanel {
 
     private static BufferedImage string(String string) {
         return drawString(string, font, black);
+    }
+
+    private static BufferedImage border(BufferedImage image, int size, boolean soon) {
+        int width = image.getWidth() + size * 2;
+        int height = image.getHeight() + size * 2;
+        return createImage(width, height, g -> {
+            if (soon) {
+                g.setColor(orange);
+                g.drawRect(1, 1, width - 3, height - 3);
+                g.drawRect(2, 2, width - 5, height - 5);
+                g.setColor(black);
+                g.drawRect(0, 0, width - 1, height - 1);
+            }
+            g.drawImage(image, size, size, null);
+        });
+    }
+
+    private static long daysUntilNext(LocalDate currentDate, LocalDate birthday) {
+        long between = DAYS.between(currentDate, birthday);
+        return between >= 0 ? between : daysUntilNext(currentDate, birthday.withYear(currentDate.getYear() + 1));
     }
 }
